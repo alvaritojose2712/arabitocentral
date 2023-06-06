11b2fd426d302fac72d84996c7974663e1e7d546
Alvaro Ospino <34920387+alvaritojose2712@users.noreply.github.com>
2023-06-06 16:49:01 -0400

    dd

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,15 +37,15 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
-                'encrypted' => true,
-                'host' => '127.0.0.1',
+                'useTLS' => false,
+                'encrypted' => false,
+                'host' => 'titanio.lat',
                 'port' => 6001,
-                'scheme' => 'https',
-                'curl_options' => [
+                'scheme' => 'http',
+                /* 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
-                ],
+                ], */
             ],
         ],
 
