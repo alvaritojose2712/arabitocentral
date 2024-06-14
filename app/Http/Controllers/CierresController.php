2ff9aad9f9b03246162f7071cb1732abb827ce10
Alvaro Ospino <34920387+alvaritojose2712@users.noreply.github.com>
2024-06-14 18:53:27 -0400

    aa

--- app/Http/Controllers/CierresController.php
+++ app/Http/Controllers/CierresController.php
@@ -25,7 +25,6 @@ class CierresController extends Controller
 
     function setAll(Request $req) {
         $sendestadisticasVentareq = $req->sendestadisticasVenta;
-        return $sendestadisticasVentareq;
         $sendestadisticasVenta = json_decode(gzuncompress(base64_decode($sendestadisticasVentareq)),true);
         //return $sendestadisticasVenta;
 
