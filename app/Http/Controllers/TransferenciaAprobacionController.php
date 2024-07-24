2f55b55613ed226a0d7a4d8bcddd76b20c504da4
Alvaro Ospino <34920387+alvaritojose2712@users.noreply.github.com>
2024-07-24 09:15:31 -0400

    dsds

--- app/Http/Controllers/TransferenciaAprobacionController.php
+++ app/Http/Controllers/TransferenciaAprobacionController.php
@@ -100,7 +100,7 @@ class TransferenciaAprobacionController extends Controller
     
             switch ($tipo) {
                 case 'delete':
-                    $mov = transferencia_aprobacion::find($id)->delete();
+                    //$mov = transferencia_aprobacion::find($id)->delete();
                     break;
                     
                 case 'aprobar':
