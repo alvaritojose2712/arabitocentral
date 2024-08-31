107560bfb99226260e45960f787ee120eef12f52
Alvaro Ospino <34920387+alvaritojose2712@users.noreply.github.com>
2024-08-31 11:40:57 -0400

    dfd

--- app/Http/Controllers/BancosController.php
+++ app/Http/Controllers/BancosController.php
@@ -494,7 +494,7 @@ class BancosController extends Controller
         array_multisort(array_column($xfechaCuadre, "banco_codigo"), SORT_ASC, $xfechaCuadre);
 
        
-        $porliquidar = $puntosybiopagos->get()->marge($movsnoreportados)->merge($movsyareportados)->toArray();
+        $porliquidar = $puntosybiopagos->get()->merge($movsnoreportados)->merge($movsyareportados)->toArray();
         array_multisort(array_column($porliquidar, $columnOrder), $order=="desc"? SORT_DESC: SORT_ASC, $porliquidar);
 
         return [
