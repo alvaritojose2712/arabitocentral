22209098535e894481c062bc404863debcd1f62d
Alvaro Ospino <34920387+alvaritojose2712@users.noreply.github.com>
2024-08-31 10:57:08 -0400

    fdfd

--- app/Http/Controllers/BancosController.php
+++ app/Http/Controllers/BancosController.php
@@ -275,7 +275,7 @@ class BancosController extends Controller
 
         $mergebs = array_merge($bs1,$bs2, array_merge($bs3,$bs4,$bs5));
 
-        $puntosmascuentas = array_merge($puntosybiopagos->get()->toArray(), $mergebs);
+        $puntosmascuentas = array_merge($puntosybiopagos->get()->merge($movsnoreportados)->merge($movsyareportados)->toArray(), $mergebs);
         array_multisort(array_column($puntosmascuentas, $columnOrder), $order=="desc"? SORT_DESC: SORT_ASC, $puntosmascuentas);
         
         $xbanco = collect($puntosmascuentas)->map(function ($q) {
@@ -505,7 +505,7 @@ class BancosController extends Controller
 
             "movsnoreportadosTotal" => $movsnoreportadosTotal, 
             "movsnoreportadosTotalsum" => $movsnoreportadosTotalsum, 
-            "xliquidar" => $puntosybiopagos->get()->merge($movsnoreportados)->merge($movsyareportados), 
+            "xliquidar" => $puntosmascuentas, 
             "estado" => true,
             "view" => $subviewAuditoria,
         ];
