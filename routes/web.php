f12d58e3afd11469ce1c5c22898475ce59bb27ff
Alvaro Ospino <34920387+alvaritojose2712@users.noreply.github.com>
2024-09-11 16:01:27 -0400

    fdf

--- routes/web.php
+++ routes/web.php
@@ -84,7 +84,7 @@ use App\Models\catcajas;
 Route::post('login', [home::class,"login"]);
 Route::get('', [home::class,"index"]);
 
-Route::middleware('login')->group(function () {
+Route::group(['middleware' => ['login']],function () {
    
 
     Route::get('setmovsjunio', [PuntosybiopagosController::class,"setmovsjunio"]);
